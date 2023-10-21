1ad711cdc1956980323535d341db3b91c04f57d8
StrawberryAkai <zihun1392@gmail.com>
2023-10-21 12:01:46 -0700

    modify hero

--- Superhero-Simulator/armor.py
+++ Superhero-Simulator/armor.py
@@ -1,2 +1,13 @@
+import random as r
 class Armor:
-    def __init__(self, name, max_damage):
+    def __init__(self, name, max_block):
+        self.name = name
+        self.max_block = max_block
+    
+    def block(self):
+        block_value = r.randint(0, self.max_block)
+        return block_value
+    
+if __name__ == "__main__":
+    armor1 = Armor("diamond armor", 150)
+    print(armor1.block())
--- Superhero-Simulator/hero.py
+++ Superhero-Simulator/hero.py
@@ -1,5 +1,8 @@
 # hero.py
 import random as r
+from ability import Ability
+from armor import Armor
+
 class Hero:
   # We want our hero to have a default "starting_health",
   # so we can set that in the function header.
@@ -18,6 +21,13 @@ class Hero:
         # when a hero is created, their current health is
         # always the same as their starting health (no damage taken yet!)
         self.current_health = starting_health
+        
+        #set armors to empty array
+        self.armors = []
+        
+        #set ability to empty array
+        self.abilities = []
+        
 
     def battle(self, opponent):
         ''' Current Hero will take turns fighting the opponent hero passed in.
@@ -29,11 +39,36 @@ class Hero:
         winer = r.choice([self, opponent])
         return winer.name
         
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+        
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.max_damage
+        return total_damage
+    
+    def defend(self):
+        total_block = 0
+        for block in self.armors:
+            total_block += block.max_block
+        return total_block
+
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
     my_hero = Hero("Grace Hopper", 200)
     opponent = Hero("Yuxuan", 200)
+    
+    lighning = Ability("Thunder Bolt", 25)
+    my_hero.add_ability(lighning)
+    
+    fire = Ability("Fire Ball", 50)
+    my_hero.add_ability(fire)
+    
+    for i in my_hero.abilities:
+        print(i.name)
     print(my_hero.name)
     print(my_hero.current_health)
     print(my_hero.battle(opponent))
+    print(my_hero.attack())
